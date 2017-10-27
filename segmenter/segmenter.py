070ef928ff007d941ea3a5ac8e7c00c32390c8f9
AnastasiaBuianova <anastasia.d.buianova@gmail.com>
2017-10-27 20:35:52 +0300

    Add files via upload

---
+++ segmenter/segmenter.py
@@ -0,0 +1,41 @@
+import sys
+import re
+import nltk
+from nltk import tokenize
+
+#Первый вариант - нечестный
+file = open('wiki.txt', 'r', encoding='utf-8')
+
+for paragraph in file:    
+    s = tokenize.sent_tokenize(paragraph)
+    paragraphs.append(s)
+    print (paragraphs)
+
+file.close()
+
+#Второй вариант - честный
+
+file = open('wiki.txt', 'r', encoding='utf-8')
+lines = []
+abbreviations = {'с. ш.':'#1#', 'з. д.':'#2#'}
+abbreviations2 = {'т.є.':'#1#', 'т. є.':'#2#', 'т. з.':'#3#', 'т.з.':'#4#', 'ітд.':'#5#'}
+
+for paragraph in file:
+    paragraph = re.sub(r'([0-9]+)\.', r'\1[ORD]', paragraph)
+    for key, value in abbreviations.items():
+        paragraph = paragraph.replace(key, value)
+    lines.append(paragraph)
+    #print('###', lines)
+    
+    for line in lines:
+        line = re.sub('[.]', '.$', line)
+        sentences = line.split('$ ')        
+    
+        for s in sentences:
+            s = re.sub(r'\[ORD\]', '.', s)
+            for key, value in abbreviations.items():
+                s = s.replace(value, key)
+                print (s)
+
+
+file.close()
